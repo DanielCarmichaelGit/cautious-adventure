f2340fbea6dd971cf956355d4aa47b7229614e23
Daniel Carmichael <danielfcarmichael@gmail.com>
2024-05-05 17:16:03 -0700

    add return for random client_id

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-const { Pool } = require('pg');
+const express = require("express");
+const cors = require("cors");
+const { Pool } = require("pg");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,10 +10,9 @@ app.use(cors());
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false
-  }
+    rejectUnauthorized: false,
+  },
 });
-
 
 const defaultPageSize = 250;
 
@@ -21,8 +20,14 @@ const defaultPageSize = 250;
 const getOffset = (page, pageSize) => (page - 1) * pageSize;
 
 // Define your API routes here
-app.get('/api/time-series', (req, res) => {
-  const { marketType, startDate, endDate, page = 1, pageSize = defaultPageSize } = req.query;
+app.get("/api/time-series", (req, res) => {
+  const {
+    marketType,
+    startDate,
+    endDate,
+    page = 1,
+    pageSize = defaultPageSize,
+  } = req.query;
   const offset = getOffset(page, pageSize);
   const query = `
     SELECT DATE_TRUNC('day', accepted_datetime_utc) AS date,
@@ -34,18 +39,18 @@ app.get('/api/time-series', (req, res) => {
     LIMIT $4 OFFSET $5;
   `;
   const values = [marketType, startDate, endDate, pageSize, offset];
-  
+
   pool.query(query, values, (err, result) => {
     if (err) {
-      console.error('Error executing query:', err);
-      res.status(500).json({ error: 'Internal server error' });
+      console.error("Error executing query:", err);
+      res.status(500).json({ error: "Internal server error" });
     } else {
       res.json(result.rows);
     }
   });
 });
 
-app.get('/api/sports', (req, res) => {
+app.get("/api/sports", (req, res) => {
   const { page = 1, pageSize = defaultPageSize } = req.query;
   const offset = getOffset(page, pageSize);
   const query = `
@@ -54,11 +59,11 @@ app.get('/api/sports', (req, res) => {
     LIMIT $1 OFFSET $2;
   `;
   const values = [pageSize, offset];
-  
+
   pool.query(query, values, (err, result) => {
     if (err) {
-      console.error('Error executing sports query:', err);
-      res.status(500).json({ error: 'Internal server error' });
+      console.error("Error executing sports query:", err);
+      res.status(500).json({ error: "Internal server error" });
     } else {
       const sports = result.rows.map((row) => row.sport);
       res.json(sports);
@@ -66,7 +71,7 @@ app.get('/api/sports', (req, res) => {
   });
 });
 
-app.get('/api/stat-types', (req, res) => {
+app.get("/api/stat-types", (req, res) => {
   const { page = 1, pageSize = defaultPageSize } = req.query;
   const offset = getOffset(page, pageSize);
   const query = `
@@ -75,11 +80,11 @@ app.get('/api/stat-types', (req, res) => {
     LIMIT $1 OFFSET $2;
   `;
   const values = [pageSize, offset];
-  
+
   pool.query(query, values, (err, result) => {
     if (err) {
-      console.error('Error executing stat types query:', err);
-      res.status(500).json({ error: 'Internal server error' });
+      console.error("Error executing stat types query:", err);
+      res.status(500).json({ error: "Internal server error" });
     } else {
       const statTypes = result.rows.map((row) => row.stat_type);
       res.json(statTypes);
@@ -87,7 +92,7 @@ app.get('/api/stat-types', (req, res) => {
   });
 });
 
-app.get('/api/dimensional-analysis', (req, res) => {
+app.get("/api/dimensional-analysis", (req, res) => {
   const { dimension, page = 1, pageSize = defaultPageSize } = req.query;
   const offset = getOffset(page, pageSize);
   const query = `
@@ -98,13 +103,36 @@ app.get('/api/dimensional-analysis', (req, res) => {
     LIMIT $1 OFFSET $2;
   `;
   const values = [pageSize, offset];
-  
+
   pool.query(query, values, (err, result) => {
     if (err) {
-      console.error('Error executing dimensional analysis query:', err);
-      res.status(500).json({ error: 'Internal server error' });
+      console.error("Error executing dimensional analysis query:", err);
+      res.status(500).json({ error: "Internal server error" });
     } else {
       res.json(result.rows);
+    }
+  });
+});
+
+app.get("/api/client-id", (req, res) => {
+  const query = `
+    SELECT client_id
+    FROM bet_transactions
+    ORDER BY RANDOM()
+    LIMIT 1;
+  `;
+
+  pool.query(query, (err, result) => {
+    if (err) {
+      console.error("Error executing client ID query:", err);
+      res.status(500).json({ error: "Internal server error" });
+    } else {
+      if (result.rows.length > 0) {
+        const clientId = result.rows[0].client_id;
+        res.json({ clientId });
+      } else {
+        res.status(404).json({ error: "No client IDs found" });
+      }
     }
   });
 });
